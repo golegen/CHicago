400e81e85ae1de76862e2c54c4873ad73096b31d
CHOSTeam <italolmm2018@outlook.com>
2018-10-24 12:55:56 -0300

    I need to rewrite the LoaderGetMemoryMap function, it's crashing VMWare and VirtualBox...

--- boot/bootmgr/arch/x86/start.s
+++ boot/bootmgr/arch/x86/start.s
@@ -1,7 +1,7 @@
 // File author is Ítalo Lima Marconato Matias
 //
 // Created on October 22 of 2018, at 15:31 BRT
-// Last edited on October 23 of 2018, at 19:26 BRT
+// Last edited on October 23 of 2018, at 12:53 BRT
 
 .code16
 
@@ -10,17 +10,13 @@
 LoaderEntry:
 	cli
 	
-	mov $0, %ax							// Setup segments
+	xor %ax, %ax						// Setup segments
 	mov %ax, %ds
 	mov %ax, %es
-	mov $0x9000, %ax
 	mov %ax, %ss
-	mov $0xFFFF, %sp
+	mov $0x9FFFF, %sp
 	
 	movb %dl, (LoaderBootDrive)
-	
-	mov $0x1000, %di					// Save the memory map
-	call LoaderGetMemoryMap
 	
 	call LoaderEnableA20				// Enable the A20 gate
 	
@@ -45,82 +41,37 @@ LoaderEntry:
 
 .code16
 
-LoaderGetMemoryMap:
-	pusha								// Save all registers
-	xor %ebx, %ebx						// EBX must be 0 to start
-	xor %bp, %bp						// We're going to keep the entry count in BP
-	mov $0x534D4150, %edx				// Place "SMAP" into EDX
-	mov $0xE820, %eax
-	movl $1, %es:20(%di)				// Force a valid ACPI 3.x entry
-	mov $24, %ecx						// Ask for 24 bytes
-	int $0x15
-	jc 5f								// Unsupported function?
-	mov $0x534D4150, %edx				// Place "SMAP" into EDX
-	cmp %edx, %eax						// On success EAX will be "SMAP"
-	jne 5f
-	test %ebx, %ebx						// 1 entry long list?
-	je 5f
-	jmp 2f
-1:
-	mov $0xE820, %eax
-	movl $1, %es:20(%di)				// Force a valid ACPI 3.x entry
-	mov $24, %ecx
-	int $0x15
-	jc 1b
-	mov $0x534D4150, %edx				// Place "SMAP" into EDX
-2:
-	jcxz 4f								// Skip 0-length entries
-	cmp $20, %cl						// Got a 24 byte ACPI 3.x response?
-	jbe 3f
-	testb $1, %es:20(%di)
-	je 4f
-3:
-	mov %es:8(%di), %ecx
-	or %es:12(%di), %ecx
-	jz 4f
-	inc %bp
-	add $24, %bp
-4:
-	test %ebx, %ebx						// If EBX is 0, the list is complete!
-	jne 1b
-	popa
-	clc
-	ret
-5:
-	popa
-	stc
-	ret
-
 LoaderEnableA20:
-	pusha								// Save all registers
+	pusha								// Save all the registers
 	
 	call 1f								// Disable keyboard
 	mov $0xAD, %al
 	out %al, $0x64
-	call 1f
 	
-	mov $0xD0, %al						// Tell controller to read output port
+	call 1f								// Tell the controller to read the output port
+	mov $0xD0, %al
 	out %al, $0x64
-	call 2f
 	
-	in $0x64, %al						// Get output port data and store it
+	call 2f								// Get the output port data and store it
+	in $0x60, %al
 	push %eax
-	call 1f
 	
-	mov $0xD1, %al						// Tell controller to write output port
+	call 1f								// Tell controller to write to the output port
+	mov $0xD1, %al
 	out %al, $0x64
-	call 1f
 	
-	pop %eax							// Enable the A20 gate
+	call 1f								// Enable A20
+	pop %eax
 	or $2, %al
 	out %al, $0x60
 	
-	call 1f								// Re-enable keyboard
+	call 1f								// Enable the keyboard
 	mov $0xAE, %al
 	out %al, $0x64
+	
 	call 1f
 	
-	popa
+	popa								// Restore all the registers
 	ret
 1:
 	in $0x64, %al
@@ -128,9 +79,14 @@ LoaderEnableA20:
 	jnz 1b
 	ret
 2:
+	call 1b
+	mov $0xD0, %al
+	out %al, $0x64
+3:
 	in $0x64, %al
 	test $1, %al
-	jz 2b
+	jz 3b
+	in $0x60, %al
 	ret
 
 LoaderInstallGDT:
